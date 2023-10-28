809f6d239d7e07937312257c0f17ffece981569e
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-10-29 05:46:24 +0800

    Add comment

--- example.java
+++ example.java
@@ -1,5 +1,9 @@
 public class example{
     public static void main(String[] args){
         System.out.println("Hello World");
+        
+        int x = 200;
+
+        System.out.println(x);
     }
 }
